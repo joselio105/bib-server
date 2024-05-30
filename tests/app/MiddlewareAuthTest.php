8de77eb96e6395b1fbe9b9acfc53342d6035f260
José Hélio Veríssimoo Jr <jose.helio@gmail.com>
2024-05-29 23:15:54 -0300

    test: Use Pest framework

--- tests/app/MiddlewareAuthTest.php
+++ tests/app/MiddlewareAuthTest.php
@@ -1,8 +1,5 @@
 <?php
 
-namespace plugse\test\app;
-
-use PHPUnit\Framework\TestCase;
 use plugse\server\core\errors\PermitionDeniedError;
 use plugse\server\core\errors\PermitionIncorrectError;
 use plugse\server\core\errors\TokenDecodeError;
@@ -16,146 +13,141 @@ use plugse\server\core\infra\http\routes\Router;
 use plugse\test\mocks\controllers\BarController;
 use plugse\server\infra\http\middlewares\AuthMiddleware;
 
-class MiddlewareAuthTest extends TestCase
-{
-    public function testAuthPass()
-    {        
-        $token = Crypto::CreateJWT(2)['token'];
-        $request = (new Request)
-            ->setUri('bar/123')
-            ->setHttpMethod('post')
-            ->setHeader([
-                'Authorization'=>"Bearer {$token}"
-            ]
-        );
 
-        $router = new Router(
-            $request, 
-            [new Route('bar/:id', 'POST', BarController::class, 'index', [AuthMiddleware::class])]
-        );
-        $route = $router->getRoute();
-        $request->setParams($router->getParams($route));
+test('auth pass', function () {
+    if(defined('SETTING_FILE')){
+        define('SETTINGS_FILE', './src/settings/main.php');
+    }
+    $token = Crypto::CreateJWT(2)['token'];
+    $request = (new Request)
+        ->setUri('bar/123')
+        ->setHttpMethod('post')
+        ->setHeader([
+            'Authorization'=>"Bearer {$token}"
+        ]
+    );
 
-        foreach($route->middwares as $middleware){
-            Classname::runMethod($middleware, 'run', [$request]);
-        }
-        $response = Classname::runMethod(
-            $route->controller,
-            $route->action,
-            [],
-            [$request]
-        );
-        
-        $this->assertInstanceOf(Response::class, $response);
-        $this->assertEquals(200, http_response_code());
-    
+    $router = new Router(
+        $request, 
+        [new Route('bar/:id', 'POST', BarController::class, 'index', [AuthMiddleware::class])]
+    );
+    $route = $router->getRoute();
+    $request->setParams($router->getParams($route));
+
+    foreach($route->middwares as $middleware){
+        Classname::runMethod($middleware, 'run', [$request]);
+    }
+    $response = Classname::runMethod(
+        $route->controller,
+        $route->action,
+        [],
+        [$request]
+    );
+
+    expect($response)->toBeInstanceOf(Response::class);
+    expect(http_response_code())->toEqual(200);
+});
+
+test('fail permition denied', function () {
+    $this->expectException(PermitionDeniedError::class);
+    $request = (new Request)
+        ->setUri('bar/123')
+        ->setHttpMethod('post');
+
+    $router = new Router(
+        $request, 
+        [new Route('bar/:id', 'POST', BarController::class, 'index', [AuthMiddleware::class])]
+    );
+    $route = $router->getRoute();
+    $request->setParams($router->getParams($route));
+
+    foreach($route->middwares as $middleware){
+        Classname::runMethod($middleware, 'run', [$request]);
     }
 
-    public function testFailPermitionDenied()
-    {        
-        $this->expectException(PermitionDeniedError::class);
-        $request = (new Request)
-            ->setUri('bar/123')
-            ->setHttpMethod('post');
+    Classname::runMethod(
+        $route->controller,
+        $route->action,
+        [],
+        [$request]
+    );
+});
 
-        $router = new Router(
-            $request, 
-            [new Route('bar/:id', 'POST', BarController::class, 'index', [AuthMiddleware::class])]
-        );
-        $route = $router->getRoute();
-        $request->setParams($router->getParams($route));
+test('fail permition incorrect', function () {
+    $this->expectException(PermitionIncorrectError::class);
+    $request = (new Request)
+        ->setUri('bar/123')
+        ->setHttpMethod('post')
+        ->setHeader(['Authorization'=>'xxx']);
 
-        foreach($route->middwares as $middleware){
-            Classname::runMethod($middleware, 'run', [$request]);
-        }
+    $router = new Router(
+        $request, 
+        [new Route('bar/:id', 'POST', BarController::class, 'index', [AuthMiddleware::class])]
+    );
+    $route = $router->getRoute();
+    $request->setParams($router->getParams($route));
 
-        Classname::runMethod(
-            $route->controller,
-            $route->action,
-            [],
-            [$request]
-        );
+    foreach($route->middwares as $middleware){
+        Classname::runMethod($middleware, 'run', [$request]);
     }
 
-    public function testFailPermitionIncorrect()
-    {        
-        $this->expectException(PermitionIncorrectError::class);
-        $request = (new Request)
-            ->setUri('bar/123')
-            ->setHttpMethod('post')
-            ->setHeader(['Authorization'=>'xxx']);
+    Classname::runMethod(
+        $route->controller,
+        $route->action,
+        [],
+        [$request]
+    );
+});
 
-        $router = new Router(
-            $request, 
-            [new Route('bar/:id', 'POST', BarController::class, 'index', [AuthMiddleware::class])]
-        );
-        $route = $router->getRoute();
-        $request->setParams($router->getParams($route));
+test('fail token decode', function () {
+    $this->expectException(TokenDecodeError::class);
+    $request = (new Request)
+        ->setUri('bar/123')
+        ->setHttpMethod('post')
+        ->setHeader(['Authorization'=>"Bearer a.b.c"]);
 
-        foreach($route->middwares as $middleware){
-            Classname::runMethod($middleware, 'run', [$request]);
-        }
+    $router = new Router(
+        $request, 
+        [new Route('bar/:id', 'POST', BarController::class, 'index', [AuthMiddleware::class])]
+    );
+    $route = $router->getRoute();
+    $request->setParams($router->getParams($route));
 
-        Classname::runMethod(
-            $route->controller,
-            $route->action,
-            [],
-            [$request]
-        );
+    foreach($route->middwares as $middleware){
+        Classname::runMethod($middleware, 'run', [$request]);
     }
 
-    public function testFailTokenDecode()
-    {        
-        $this->expectException(TokenDecodeError::class);
-        $request = (new Request)
-            ->setUri('bar/123')
-            ->setHttpMethod('post')
-            ->setHeader(['Authorization'=>"Bearer a.b.c"]);
+    Classname::runMethod(
+        $route->controller,
+        $route->action,
+        [],
+        [$request]
+    );
+});
 
-        $router = new Router(
-            $request, 
-            [new Route('bar/:id', 'POST', BarController::class, 'index', [AuthMiddleware::class])]
-        );
-        $route = $router->getRoute();
-        $request->setParams($router->getParams($route));
+test('fail token expired', function () {
+    $this->expectException(TokenExpiredError::class);
+    $token = Crypto::CreateJWT(2, Crypto::getTimestamp('2024-01-01'))['token'];
+    $request = (new Request)
+        ->setUri('bar/123')
+        ->setHttpMethod('post')
+        ->setHeader(['Authorization'=>"Bearer {$token}"]);
 
-        foreach($route->middwares as $middleware){
-            Classname::runMethod($middleware, 'run', [$request]);
-        }
+    $router = new Router(
+        $request, 
+        [new Route('bar/:id', 'POST', BarController::class, 'index', [AuthMiddleware::class])]
+    );
+    $route = $router->getRoute();
+    $request->setParams($router->getParams($route));
 
-        Classname::runMethod(
-            $route->controller,
-            $route->action,
-            [],
-            [$request]
-        );
+    foreach($route->middwares as $middleware){
+        Classname::runMethod($middleware, 'run', [$request]);
     }
 
-    public function testFailTokenExpired()
-    {        
-        $this->expectException(TokenExpiredError::class);
-        $token = Crypto::CreateJWT(2, Crypto::getTimestamp('2024-01-01'))['token'];
-        $request = (new Request)
-            ->setUri('bar/123')
-            ->setHttpMethod('post')
-            ->setHeader(['Authorization'=>"Bearer {$token}"]);
-
-        $router = new Router(
-            $request, 
-            [new Route('bar/:id', 'POST', BarController::class, 'index', [AuthMiddleware::class])]
-        );
-        $route = $router->getRoute();
-        $request->setParams($router->getParams($route));
-
-        foreach($route->middwares as $middleware){
-            Classname::runMethod($middleware, 'run', [$request]);
-        }
-
-        Classname::runMethod(
-            $route->controller,
-            $route->action,
-            [],
-            [$request]
-        );
-    }
-}
+    Classname::runMethod(
+        $route->controller,
+        $route->action,
+        [],
+        [$request]
+    );
+});
